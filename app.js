22c82285d1addcb72c15b247afe3685f66aed8b2
NikolajChristianMoeller <nikolajchristianmoeller@gmail.com>
2023-02-03 09:24:44 +0100

    Hej
    Jeg fik den til at sige hej

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ document.body.innerHTML += "Hello JavaScript!";
 console.log("Hello Dat.js");
 console.log("Hi Students!");
 console.log("This is your first JavaScript App");
+function sayHello() {
+    document.querySelector("h3").textContent = "Hej" + document.querySelector("input").value;
+}
+document.querySelector("button").addEventListener("click", sayHello);
