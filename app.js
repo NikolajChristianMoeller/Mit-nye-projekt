16efa9837989f8c0d86e6e94ffacbd3f0d336a01
NikolajChristianMoeller <nikolajchristianmoeller@gmail.com>
2023-02-06 12:58:24 +0100

    Hejhej
    Jeg skal save

--- app.js
+++ app.js
@@ -7,3 +7,5 @@ function sayHello() {
     document.querySelector("h3").textContent = "Hej" + document.querySelector("input").value;
 }
 document.querySelector("button").addEventListener("click", sayHello);
+document.querySelector("#name")
+document.querySelector("email")
